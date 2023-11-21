d2bc7bd3b3d0888e2514783e3cd7755d2a55854f
tejas-rathi05 <113531266+tejas-rathi05@users.noreply.github.com>
2023-11-22 02:09:43 +0530

    Uploaded Files

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import SuperQuality from './SuperQuality'
+import Subscribe from './Subscribe' 
+
+export {
+  Hero,
+  PopularProducts,
+  CustomerReviews,
+  Footer,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+  Subscribe
+}
